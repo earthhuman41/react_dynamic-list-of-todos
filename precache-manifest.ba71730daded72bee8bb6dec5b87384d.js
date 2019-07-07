4d129ca6e2661e8854d8c06363f3169394ac6244
alex <earthhuman41@gmail.com>
2019-07-07 17:46:17 +0300

    Updates

---
+++ precache-manifest.ba71730daded72bee8bb6dec5b87384d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6777068173e6eda028324e2b4777cc3d",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "6e8b0da50fba9bc623a3",
+    "url": "/react_dynamic-list-of-todos/static/css/main.77e92c71.chunk.css"
+  },
+  {
+    "revision": "e799e2455381a5dda283",
+    "url": "/react_dynamic-list-of-todos/static/js/2.405cca3d.chunk.js"
+  },
+  {
+    "revision": "6e8b0da50fba9bc623a3",
+    "url": "/react_dynamic-list-of-todos/static/js/main.d061012b.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
